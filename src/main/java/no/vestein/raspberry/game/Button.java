b8640364f8865e10e4695e27935ba56551293e3e
WoodStone <golfflames@gmail.com>
2015-05-06 22:44:28 +0200

    Changed button to enum

--- src/main/java/no/vestein/raspberry/game/Button.java
+++ src/main/java/no/vestein/raspberry/game/Button.java
@@ -8,10 +8,16 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
-public class Button {
+public enum Button {
+	
+	UP(RaspiPin.GPIO_00, 0, -1),
+	DOWN(RaspiPin.GPIO_02, 0, 1),
+	RIGHT(RaspiPin.GPIO_03, 1, 0),
+	LEFT(RaspiPin.GPIO_04, -1, 0),;
 	
 	private final GpioController gpio = GpioFactory.getInstance();
 	private final GpioPinDigitalInput buttonPin;
@@ -20,7 +26,7 @@ public class Button {
 	
 	private List<ButtonListener> listeners = new ArrayList<>();
 	
-	public Button(Pin pin, int dirx, int diry) {
+	private Button(Pin pin, int dirx, int diry) {
 		this.dirx = dirx;
 		this.diry = diry;
 		buttonPin = gpio.provisionDigitalInputPin(pin, PinPullResistance.PULL_DOWN);
--- src/main/java/no/vestein/raspberry/game/Main.java
+++ src/main/java/no/vestein/raspberry/game/Main.java
@@ -3,26 +3,18 @@ package no.vestein.raspberry.game;
 import java.io.Console;
 
 import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.RaspiPin;
 
 
 public class Main {
 	
-	
-	
 	public static void main(String[] args) throws InterruptedException {
-		
-		Button up = new Button(RaspiPin.GPIO_00, 0, -1);
-		Button down = new Button(RaspiPin.GPIO_02, 0, 1);
-		Button right = new Button(RaspiPin.GPIO_03, 1, 0);
-		Button left = new Button(RaspiPin.GPIO_04, -1, 0);
 		
 		Board board = Board.getInstance();
 		Renderer renderer = new Renderer(board);
 		board.addListener(renderer);
 				
 		ButtonController buttonController = new ButtonController();
-		buttonController.addButtons(up, down, right, left);
+		buttonController.addButtons(Button.values());
 		
 		board.init();
 		
