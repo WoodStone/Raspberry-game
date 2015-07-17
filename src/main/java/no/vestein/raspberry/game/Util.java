a0299e8fbc21155044144f036a413852fba3aab2
WoodStone <golfflames@gmail.com>
2015-07-18 00:42:14 +0200

    Reuse of random object

--- src/main/java/no/vestein/raspberry/game/Util.java
+++ src/main/java/no/vestein/raspberry/game/Util.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class Util {
 
+    private static Random rand = new Random();
+
     public static int randInt(int min, int max) {
-        Random rand = new Random();
         int randNum = rand.nextInt(max - min + 1) + min;
         return randNum;
     }
